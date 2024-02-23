a4cc08919fcefd7fe7f8bda88b0329cc2db2a56e
Javier González Ramos <93089496+Javix13@users.noreply.github.com>
2024-02-23 21:06:11 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillMembers() {
+  return {
+    name: 'skillMembers',
+    members: [
+      { name: 'member1', skill: 'skill1' },
+      { name: 'member2', skill: 'skill2' },
+      { name: 'member3', skill: 'skill3' }
+    ]
+  };
+}
